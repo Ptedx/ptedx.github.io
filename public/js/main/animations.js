9476bd15a69c307a1b621459f0f0b5b8e8685dc9
Vinicius Costa <64545204+Ptedx@users.noreply.github.com>
2023-12-04 13:02:50 -0300

    Fixing animations in Safari

--- public/js/main/animations.js
+++ public/js/main/animations.js
@@ -1,8 +1,13 @@
+
 /*Fade In Right*/
 function fadeInRight(entries,observerR){
     entries.forEach((entry)=>{
         if (entry.isIntersecting){
-            entry.target.classList.add('fadeInRight')
+            gsap.to(entry.target,{
+                x:0,
+                opacity:1,
+                duration:2.5
+            })
             observerR.unobserve(entry.target)
         }
     })
@@ -10,6 +15,7 @@ function fadeInRight(entries,observerR){
 const fadeInR = document.querySelectorAll('.observeR')
 const observerR = new IntersectionObserver(fadeInRight,{threshold:0.5})
 fadeInR.forEach((div)=>{
+    gsap.set(div,{x:100})
     observerR.observe(div)
 })
 
@@ -17,7 +23,11 @@ fadeInR.forEach((div)=>{
 function fadeInLeft(entries,observer){
     entries.forEach((entry)=>{
         if (entry.isIntersecting){
-            entry.target.classList.add('fadeInLeft')
+            gsap.to(entry.target,{
+                x:0,
+                opacity:1,
+                duration: 2.5
+            })
             observer.unobserve(entry.target)
         }
     })
@@ -25,6 +35,7 @@ function fadeInLeft(entries,observer){
 const fadeInL = document.querySelectorAll('.observeL')
 const observerL = new IntersectionObserver(fadeInLeft,{threshold:0.5})
 fadeInL.forEach((div)=>{
+    gsap.set(div,{x:-100})
     observerL.observe(div)
 })
 
@@ -32,7 +43,11 @@ fadeInL.forEach((div)=>{
 function fadeInBottom(entries,observer){
     entries.forEach((entry)=>{
         if (entry.isIntersecting){
-            entry.target.classList.add('fadeInBottom')
+            gsap.to(entry.target,{
+                opacity:1,
+                y: 0,
+                duration: 2
+            })
             observer.unobserve(entry.target)
         }
     })
@@ -40,6 +55,7 @@ function fadeInBottom(entries,observer){
 const fadeInB = document.querySelectorAll('.observeB')
 const observerB = new IntersectionObserver(fadeInBottom,{threshold:0.25})
 fadeInB.forEach((div)=>{
+    gsap.set(div,{y:100})
     observerB.observe(div)
 })
 
@@ -64,3 +80,27 @@ document.addEventListener('scroll',()=>{
     }
     ticking = true
 })
+
+/*Light*/
+/*document.addEventListener('DOMContentLoaded',()=>{
+    const light = document.querySelectorAll(".light")
+    const lightm = document.querySelectorAll(".light-m")
+    light.forEach((Selected)=>{
+        gsap.fromTo(Selected,{
+            x:-100
+        },{
+            xPercent: 100,
+            duration: 1,
+            repeat:-1
+        })
+    })
+    lightm.forEach((selected=>{
+        gsap.fromTo(selected,{
+            x:-100
+        },{
+            x: "500%",
+            duration: 1,
+            repeat:-1
+        })
+    }))
+})*/
